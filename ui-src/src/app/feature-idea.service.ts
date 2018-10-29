edb1e1e0a1af03b327d260972e75158abd11903f
Pedro Pons <pons.peter@gmail.com>
2018-10-29 21:18:32 +0800

    add voting service in the feature service

--- ui-src/src/app/feature-idea.service.ts
+++ ui-src/src/app/feature-idea.service.ts
@@ -13,13 +13,23 @@ export class FeatureIdeaService {
 
   featureIdeaCreate(featureIdea: FeatureIdea) {
     return this.http.post('/fn/featureIdeas/featureIdeaCreate', featureIdea).subscribe((result) => {
-
     }, error => {
-      console.log('error');
+      console.log(error);
     });
   }
 
   featureIdeaList(): Observable<GetLinksResponse<FeatureIdea>[]> {
     return <Observable<GetLinksResponse<FeatureIdea>[]>> this.http.get('/fn/featureIdeas/featureIdeaList');
+  }
+
+  votingCreate(voting: Voting) {
+    return this.http.post('/fn/voting/votingCreate', voting).subscribe((result) => {
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  votingList(): Observable<GetLinksResponse<Voting>[]> {
+    return <Observable<GetLinksResponse<Voting>[]>> this.http.get('/fn/voting/votingList');
   }
 }
