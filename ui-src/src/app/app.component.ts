e6df7341176d498683e46de6245fe65de09b0099
Ryan How <ryan@zbit.net.au>
2018-10-29 20:13:12 +0800

    bouncy vote button

--- ui-src/src/app/app.component.ts
+++ ui-src/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, ViewChild, TemplateRef, ElementRef, ContentChild } from '@angular/core';
 import { FeatureIdeaService } from './feature-idea.service';
 import { Observable, timer, Subject } from 'rxjs';
-import { switchMap, map, shareReplay, merge } from 'rxjs/operators';
+import { switchMap, map, shareReplay, merge, throttleTime, delay, tap } from 'rxjs/operators';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
@@ -19,13 +19,19 @@ export class AppComponent implements OnInit, OnDestroy {
   columns: FeatureIdeaColumn[] = ['Feature Ideas', 'Under Consideration', 'Scoping', 'In Development', 'Completed'];
   manualRefresh = new Subject();
   modalFeatureIdea: FeatureIdea;
+  voteClick = new Subject<MouseEvent>();
   
   featureIdeaTrackBy(index: number, item: GetLinksResponse<FeatureIdea>) {
     return item.Hash;
   }
 
   constructor(private featureIdeaService: FeatureIdeaService, private modalService: BsModalService) {
-
+    this.voteClick.asObservable().pipe(
+      throttleTime(1000),
+      tap((e) => (<HTMLElement>(<HTMLElement>e.target).firstChild).classList.add('bounce')),
+      delay(1000),
+      tap((e) => (<HTMLElement>(<HTMLElement>e.target).firstChild).classList.remove('bounce'))
+    ).subscribe();
   }
 
   ngOnInit() {
@@ -50,9 +56,10 @@ export class AppComponent implements OnInit, OnDestroy {
 
   createFeatureIdeaModal() {
     this.featureIdeaService.featureIdeaCreate(this.modalFeatureIdea);
-    this.manualRefresh.next();
+    setTimeout(() => this.manualRefresh.next(), 200);
     this.modalRef.hide();
   }
+
 
   ngOnDestroy() {
   }
